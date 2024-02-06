bbe47bf90cd69a3d036ee8a60a2de780d1e547b9
Bruno Verona <bruno.verona@ecalc.com.br>
2024-02-06 10:57:42 -0300

    Novo teste

--- Deploy_On_Client-teste.ps1
+++ Deploy_On_Client-teste.ps1
@@ -2,41 +2,6 @@
 $VerbosePreference = "Continue"
 $ErrorActionPreference = "Stop"
 
-#ALTERAÇÃO 1
-
-# URL da API do GitHub para obter informações do arquivo
-$githubApiUrl = "https://api.github.com/repos/brunoverona/Deploy_teste/contents/Deploy_On_Client-teste.ps1"
-
-# Caminho local do script
-$scriptPath = "D:\Users\bruno\Desktop\Deploy_On_Client-teste.ps1"
-
-# Função para baixar e executar a versão mais recente do script
-function UpdateScript {
-    # Obtém as informações do arquivo da API do GitHub
-    $fileInfo = Invoke-RestMethod -Uri $githubApiUrl -Method Get
-
-    # Calcula o hash SHA-256 do conteúdo local
-    $hashLocal = (Get-FileHash -Path $scriptPath -Algorithm SHA256).Hash
-
-    # Converte o conteúdo do GitHub de Base64 para texto
-    $contentGitHub = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($fileInfo.content))
-
-    # Calcula o hash SHA-256 do conteúdo no GitHub
-    $hashGitHub = (Get-FileHash -Algorithm SHA256 -InputStream ([System.IO.MemoryStream]::new([System.Text.Encoding]::UTF8.GetBytes($contentGitHub)))).Hash
-
-    # Compara os hashes para verificar se há uma alteração
-    if ($hashLocal -ne $hashGitHub) {
-        # Baixa o conteúdo do arquivo diretamente do GitHub e sobrescreve o script local
-        Invoke-WebRequest -Uri $fileInfo.download_url -OutFile $scriptPath
-        Write-Host "O script foi atualizado com sucesso."
-    } else {
-        Write-Host "O script está atualizado. Continuando com a execução normal."
-    }
-}
-
-# Chama a função para verificar e atualizar o script
-UpdateScript
-
 # Usuario que executa o Deploy (deve ser o mesmo que esta logado)
 $deploy_user = "bruno"
 
