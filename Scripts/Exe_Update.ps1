104530eb17389ed3165ac66246c7fb5a0d9ac61a
brunoverona <brunoverona31@gmail.com>
2024-02-29 22:41:21 -0300

    Exe_Update funcional

--- Scripts/Exe_Update.ps1
+++ Scripts/Exe_Update.ps1
@@ -18,9 +18,31 @@ $usuario = "ecalc"
 $senha = ConvertTo-SecureString "folha" -AsPlainText -Force
 $credenciais = New-Object System.Management.Automation.PSCredential($usuario, $senha)
 
+# Executável do XStrut Console
+$xstrut_exec = "D:\Ecalc\exe\XStrutCon.exe"
+
+# Diretório do temporário para o Strut.xml
+$deploy_temp_strut = "D:\Ecalc\Exe\Strut.xml"
+
+# Bancos a serem reestruturados com Strut.xml no Client
+$alias_array = @(
+    [pscustomobject]@{Alias='OFAG';Password='masterkey';User='SYSDBA'}
+)
+
 # Iterar sobre a lista de URLs e baixar os arquivos
 foreach ($url in $urls) {
     $nomeArquivo = [System.IO.Path]::GetFileName($url)
     $caminhoCompleto = Join-Path -Path $destino -ChildPath $nomeArquivo
     Invoke-WebRequest -Uri $url -OutFile $caminhoCompleto -Credential $credenciais -AllowUnencryptedAuthentication
+
+Write-Verbose "Descompactando arquivos"
+Expand-Archive $caminhocompleto -DestinationPath $destino -Force
+Remove-Item -Path $destino* -Include *.zip
+}
+
+#Executando o Strut nos BDs do Client
+ForEach ($alias in $alias_array)
+{
+    Write-Verbose "Atualizando banco: $($alias.Alias)"
+    & $xstrut_exec -r -f $deploy_temp_strut -b $alias.Alias -u $alias.User -p $alias.Password
 }
